96f1212c70562c0149e021dd762d3a34db5611bd
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2015-11-05 18:19:07 +0000

    refactor

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # hexo-easy-edit
+**NOTE: this is currently a work-in-progress and will change as I add in the rest of the functionality over the coming week**
 
 This is a simple plugin for [Hexo](https://github.com/hexojs/hexo), a Node.js-based static site generator/blog framework.
 
 It adds one command to the Hexo command-line interface (only usable when you are somewhere inside a hexo folder):
 
 ```
-hexo edit [title] [-t | --target subfolder] [-g | --gui]
+hexo edit [title] [-f | --folder subfolder] [-g | --gui]
 ```
 
-`title` is a regular expression (case insensitive and spaces are allowed) for matching the title of a markdown file somewhere in `hexo_dir/source`
+`title` is a regular expression (case insensitive and spaces are allowed) for matching the title of a post
 
-`target` (optional) is the name of a subfolder in `hexo_dir/source` to narrow down your search some more. If the folder isn't found, this argument will be ignored.
+`folder` (optional) is (part of) the name of a subfolder in `hexo_dir/source` to narrow down your search some more.
 
 `-g` or `--gui` is an option to open files using an associated GUI editor, rather than your terminal editor set in $EDITOR
 
-This will also open any new post in your text editor when you create one using `hexo new ...`.
+Installing this will also cause any new post you create with `hexo new ...` to open automatically in your text editor.
 
 ## Installation
 
@@ -24,11 +25,11 @@ npm install --save hexo-easy-edit
 
 ## Notes
 
-- It doesn't matter where in the hexo directory you are. As long as you're in one, the plugin will find the source dir
-- The common folders `_posts` and `_drafts` are also recognized as `posts`, `post` and `_post`, and `drafts`, `draft` and `_draft`
-- If your regular expression matches more than one file, a terminal menu will load so you can choose. Use the arrow keys or vim keybindings (j/k) to pick a file. This might not work as expected in the default Windows command shell, but it should fine in more powerfull shells
+- It doesn't matter where in the hexo directory you are. As long as you're in one, your posts will be found
+- If your regular expression matches more than one file, a terminal menu will load so you can choose. Use the arrow keys or vim keybindings (j/k) to pick a file. This might not work as expected in the default Windows command shell, but it should be fine in more powerfull shells
 - By default, a file will open in your current terminal window, using whatever you've set as the global `$EDITOR` variable (you can set it in `.bashrc` or `.zshrc` if it's empty, don't forget to source the file before testing)
 - With the `gui` option set (or if you don't have an `$EDITOR` set), the file will open with `xdg-open` (linux, osx) or `start` (win)
 - A simple `hexo edit` will give you a menu with all your posts
 - All spaces in your `title` will be treated as dashes, just like Hexo does
 - This is useful primarily for myself, but might come in handy for others using Hexo who prefer to edit markdown files in their favorite local editor. Alternatively, you can use an administration plugin - check out [hexo-admin](https://github.com/jaredly/hexo-admin) or [hexo-hey](https://github.com/nihgwu/hexo-hey) if that's more up your alley
+- I will add support for filtering on tags and categories very soon
--- index.js
+++ index.js
@@ -3,24 +3,118 @@
 var open = require('open');
 var editor = process.env.EDITOR;
 var spawn = require('child_process').spawn;
+var path = require('path');
+var inquirer = require('inquirer');
 
 hexo.extend.console.register('edit', 'Edit a post or draft with your favorite $EDITOR', {
   desc: 'Edit a post or draft with your favorite $EDITOR',
-  usage: '[title] [-t|--target subfolder] [-g|--gui]',
+  usage: '[title] [-f|--folder] [-g|--gui]',
   arguments: [
     {name: 'title', desc: '(Part of) the title of a post. If more posts match this regex, a menu will be called.'},
   ],
   options: [
     {name: '-g, --gui', desc: 'Open file with associated GUI text editor. Default is to open in terminal using $EDITOR. If no $EDITOR is found, it will fall back to gui mode.'},
-    {name: '-t, --target', desc: 'Name of source subfolder to search. Will search entire source folder if no matching folder is found. Neither the leading \'_\' or the trailing \'s\' is required for specifying the _posts or _drafts folder.'},
+    {name: '-f, --folder', desc: 'Name of subfolder to filter on.'},
   ],
-}, require('./edit'));
+}, edit);
 
+// extend `hexo new` to open newly created post/draft
 hexo.on('new', function(post) {
-  if (!editor) {
-    open(file);
-  } else {
-    var edit = spawn(editor, [post.path], {stdio: 'inherit'});
-    edit.on('exit', process.exit);
+  var edit;
+  var content = post.content;
+
+  // only open a new empty post -- prevent opening on publishing an already written one
+  if (content.substr(content.indexOf('\n---\n')).length == 5) {
+    if (!editor) {
+      open(file);
+    } else {
+      edit = spawn(editor, [post.path], {stdio: 'inherit'});
+      edit.on('exit', process.exit);
+    }
   }
 });
+
+function edit(args) {
+
+  // load in the posts before processing them
+  hexo.load().then(function() {
+    var posts = hexo.locals.get('posts').sort('-date').toArray();
+
+    var sourceDir = hexo.source_dir;
+    var searchDir = sourceDir;
+    var editor = process.env.EDITOR;
+
+    var title = args._.join(' ').replace(/ /g, '-') || '';
+    var folder = args.f || args.folder || '';
+
+    var gui = args.g || args.gui || !editor;
+
+    var selected;
+    var titles;
+
+    // allow omission of leading underscore or trailing s for the common _drafts and _posts folders
+    if (/post|draft/.test(folder)) {
+      folder = (/^_/.test(folder)) ? folder : '_' + folder;
+      folder = (/s$/.test(folder)) ? folder : folder + 's';
+    }
+
+    posts = (folder) ? filterFolder(posts) : posts;
+
+    posts = (title) ? filterTitle(posts) : posts;
+
+    if (posts.length == 0) {
+      console.log('Sorry, no posts matched your query. Exiting.');
+      process.exit();
+    } else if (posts.length == 1) {
+
+      // no menu necessary if there is only one matching file
+      selected = path.join(searchDir, posts[0].source);
+      openFile(selected);
+    } else {
+
+      // get a list of titles to put in the menu -- slugs are easy because it shows the subfolder and can easily be put back together with the searchDir to open it
+      titles = posts.map(function(post) {
+        return post.source;
+      });
+
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'file',
+          message: 'Select the file you wish to edit.',
+          choices: titles,
+        },
+      ], function(answer) {
+        selected = path.join(sourceDir, answer.file);
+        openFile(selected);
+      });
+    }
+
+    // filter the posts using a subfolder if supplied
+    function filterFolder(posts) {
+      var reFolder = new RegExp(folder);
+      return posts.filter(function(post) {
+        return reFolder.test(post.source.substr(0, post.source.lastIndexOf(path.sep)));
+      });
+    }
+
+    // filter the posts with the supplied regular expression -- uses the slug to avoid capitalization and spacing shenanigans
+    function filterTitle(posts) {
+      var reTitle = new RegExp(title, 'i');
+      return posts.filter(function(post) {
+        return reTitle.test(post.slug);
+      });
+    }
+
+    // spawn process and open with associated gui or terminal editor
+    function openFile(file) {
+      if (!editor || gui) {
+        open(file);
+      } else {
+        var edit = spawn(editor, [file], {stdio: 'inherit'});
+        edit.on('exit', process.exit);
+      }
+    }
+
+  });
+};
